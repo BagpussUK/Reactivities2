3c52228faaf123355954550b5b68044417e4b10d
James Slade <James.Slade@ikirupeople.com>
2025-03-25 12:12:41 +0000

    End of section 6

--- client/src/features/activities/dashboard/ActivityList.tsx
+++ client/src/features/activities/dashboard/ActivityList.tsx
@@ -4,13 +4,11 @@ import ActivityCard from "./ActivityCard";
 type Props = {
   activities: Activity[]
   selectActivity: (id: string) => void;
-  deleteActivity: (id: string) => void;
 }
 
 export default function ActivityList({
   activities,
-  selectActivity,
-  deleteActivity }: Props) {
+  selectActivity }: Props) {
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', gap: 3 }}>
       {activities.map(activity => (
@@ -18,7 +16,6 @@ export default function ActivityList({
           key={activity.id} 
           activity={activity} 
           selectActivity={selectActivity} 
-          deleteActivity={deleteActivity}
         />
       ))}
     </Box>
